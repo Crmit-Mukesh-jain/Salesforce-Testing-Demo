5a9882b27bad89ecde5b535141c54a9eff2c61c7
Crmit-Mukesh-jain <mukesh.jain@crmit.com>
2025-03-19 15:56:27 +0530

    New Changes on 19th March

---
+++ Test Suites/TestNewSuite004.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>TestNewSuite004</description>
+   <name>TestNewSuite004</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>7ea1eb27-f7b4-4838-b5d1-6004be94839e</testSuiteGuid>
+   <testCaseLink>
+      <guid>04eabc79-ead1-4306-9e46-0480c12dc1ad</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/To Test the Keywords 0003</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
